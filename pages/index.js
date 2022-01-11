b441190754821468912e2f8a776ef2d52d932d2b
Adman94 <adman00794@gmail.com>
2022-01-11 02:12:39 -0800

    deploy attempt 1

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,28 @@ export default function Home() {
 
       <main className="main">
         <div className="profile-container">
-          <img className="image" src="/img.png" /> 
+          <img className="image" src="/img.png" alt="alt-img"/> 
           <h3 className="description">@adman94</h3>
         </div>
       </main>
 
         <div className="grid">
-          <a href="https://www.linkedin.com/in/adman94/" target="_blank" className="card">
+          <a href="https://www.linkedin.com/in/adman94/" target="_blank" rel="noopener noreferrer" className="card">
             <h3 className="sites">LinkedIn</h3>
           </a>
-          <a href="https://codeurlife.com" target="_blank" className="card">
+          <a href="https://codeurlife.com" target="_blank" rel="noopener noreferrer" className="card">
             <h3 className="sites">Website</h3>
           </a>
-          <a href="https://github.com/Adman94" target="_blank" className="card">
+          <a href="https://github.com/Adman94" target="_blank" rel="noopener noreferrer" className="card">
             <h3 className="sites">Github</h3>
           </a>
-          <a href="https://www.instagram.com/ad007man/" target="_blank" className="card">
+          <a href="https://www.instagram.com/ad007man/" target="_blank" rel="noopener noreferrer" className="card">
             <h3 className="sites">Instagram</h3>
           </a>
-          <a href="https://www.facebook.com/ad33b94/" target="_blank" className="card">
+          <a href="https://www.facebook.com/ad33b94/" target="_blank" rel="noopener noreferrer" className="card">
             <h3 className="sites">Facebook</h3>
           </a>
-          <a href="https://twitter.com/Adman00794" target="_blank" className="card">
+          <a href="https://twitter.com/Adman00794" target="_blank" rel="noopener noreferrer" className="card">
             <h3 className="sites">Twitter</h3>
           </a>
         </div>
